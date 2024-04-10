07d59e795764f291f463b1caccc3d493275274fe
Rocky <alexwaldmann2004@gmail.com>
2024-04-10 03:18:23 -0600

    Trying to fix mongo connection?

--- server/config/connection.js
+++ server/config/connection.js
@@ -5,4 +5,8 @@ mongoose.connect(
   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/googlebooks"
 );
 
+mongoose.connection.once("open", () => {
+  console.log("Connected to the Database");
+});
+
 module.exports = mongoose.connection;
