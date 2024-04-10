5ad160222ee5dab6e520bcf2b10abc6564ba06d4
Rocky <alexwaldmann2004@gmail.com>
2024-04-10 05:08:16 -0600

    Maybe

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const uri = process.env.MONGODB_URI || "mongodb://localhost:27017/googlebooks";
+const uri = process.env.MONGODB_URI;
 
 mongoose.connect(uri);
 
