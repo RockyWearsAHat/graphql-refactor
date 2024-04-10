5602ba4f114861b19965776e0efee7721054b314
Rocky <alexwaldmann2004@gmail.com>
2024-04-10 04:09:20 -0600

    Please

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/googlebooks"
+  process.env.REACT_APP_MONGODB_URI || "mongodb://127.0.0.1:27017/googlebooks"
 );
 
 mongoose.connection.once("open", async () => {
