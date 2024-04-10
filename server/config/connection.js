2cb16ba2f7dfca29d878a477c076390c678dd113
Rocky <alexwaldmann2004@gmail.com>
2024-04-10 05:22:17 -0600

    I think fixed, mostly due to my own stupidity and running a static site and not a web service

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const uri = process.env.MONGODB_URI;
+const uri = process.env.MONGODB_URI || "mongodb://localhost:27017/googlebooks";
 
 mongoose.connect(uri);
 
