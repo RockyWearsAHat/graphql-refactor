882bc19343db87bef2606e4129d5d2e110a8c4a8
Rocky <alexwaldmann2004@gmail.com>
2024-04-10 04:18:05 -0600

    This is the actually the worst

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,6 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
 
-const connectionURI = process.env.MONGODB_URI
-  ? process.env.MONGODB_URI
-  : "mongodb://127.0.0.1:27017/googlebooks";
-mongoose.connect(connectionURI);
+mongoose.connect(process.env.MONGODB_URI);
 
 mongoose.connection.once("open", async () => {
   const kittySchema = new mongoose.Schema({
