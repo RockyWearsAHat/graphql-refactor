01ba4c378af138085bcc25e637a742073631457a
Rocky <alexwaldmann2004@gmail.com>
2024-04-10 04:00:53 -0600

    Please

--- server/config/connection.js
+++ server/config/connection.js
@@ -5,7 +5,15 @@ mongoose.connect(
   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/googlebooks"
 );
 
-mongoose.connection.once("open", () => {
+mongoose.connection.once("open", async () => {
+  const kittySchema = new mongoose.Schema({
+    name: String,
+  });
+
+  const Kitten = mongoose.model("Kitten", kittySchema);
+
+  await Kitten.create({ name: "Silence" });
+
   console.log("Connected to the Database");
 });
 
